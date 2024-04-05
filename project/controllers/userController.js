e937ee8d4a8e0153cff049ba03d12b1de037b0c3
sasha-1234@ <ja4349950@gmail.com>
2024-04-06 01:03:53 +0200

    registration implemented

--- project/controllers/userController.js
+++ project/controllers/userController.js
@@ -1,7 +1,24 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const validate = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
+exports.getRegistration = (req, res) => {
+  res.render("registration");
+};
 
+exports.postRegistration = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+      role: "user",
+    });
 
-exports.registration = async(req,res) => {
-
+    await user.save();
+    res.redirect("/user/login");
+  } catch (error) {
+    next(error);
+  }
 };
--- project/routes/userRouter.js
+++ project/routes/userRouter.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controllers/userController');
+const {validateReg} = require('../utils/validation');
 
 /* GET home page. */
 userRouter.get('/',(req,res)=>{
   res.render('main', { title: 'Welcome' });
 });
 
+userRouter.post('/registration',validateReg,userController.postRegistration);
 
+userRouter.get('/registration',userController.getRegistration);
 
 module.exports = userRouter;
