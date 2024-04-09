8ccd7579dd319e2f875a5f37f2f31eb6a8c6b7ab
sasha-1234@ <ja4349950@gmail.com>
2024-04-09 22:04:41 +0200

    message routes

--- project/routes/MessageRoter.js
+++ project/routes/MessageRoter.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const MsgRouter = express.Router();
+const message = require('../controllers/msgController');
+
+const {isAuth,isMember} = require('../utils/middleware');
 
 /* GET msg's listing. */
 MsgRouter.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+MsgRouter.get('/write',isAuth,isMember,message.getNew);
+
+MsgRouter.post('/write',isAuth,isMember,message.postNew);
 
 module.exports = MsgRouter;
